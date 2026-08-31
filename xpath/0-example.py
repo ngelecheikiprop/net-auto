9cc159db5c7dea5677c7e6dff11913945a231fc9
ngelechei.kiprop <ngelechei.kiprop@gmail.com>
2026-08-31 16:36:12 +0300

    try parent with xpath

---
+++ xpath/0-example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#from jnpr.junos import Device
+#from sys import argv
+#print(argv)
+
+from lxml import etree
+
+xml = """
+<root>
+<interface>
+    <name>ge-0/0/0</name>
+    <status>up</status>
+</interface>
+<interface>
+<name>ge-0/0/1</name>
+</interface>
+</root>
+"""
+
+root = etree.fromstring(xml)
+#print(type(root))
+result = root.xpath("./interface/name")
+for node in result:
+    new_node = node.xpath("..")
+    for x in new_node:
+        print(etree.tostring(x, pretty_print=True).decode())
+#print(etree.tostring(result, pretty_print=True).decode())
+#print(result)
