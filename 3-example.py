0e3e52aa1892b0957bd2c9a250b01352f100dc3c
ngelechei.kiprop <ngelechei.kiprop@gmail.com>
2026-09-04 14:01:09 +0300

    lets align

--- 3-example.py
+++
@@ -1,8 +0,0 @@
-from jnpr.junos import Device 
-from jnpr.junos.utils.config import Config
-
-with Device(host="10.100.100.2", user="lab", password="lab123") as dev:
-  with Config(dev) as cu:
-    cu.load("set system host-name router1", format="set")
-    cu.commit()
-    print("hostname updated")
--- pyez/1-example.py
+++ pyez/1-example.py
@@ -1,9 +1,7 @@
+"""get all interfaces"""
 from jnpr.junos import Device
 from lxml import etree
 with Device(host="10.100.100.2", user="lab", password="lab123") as dev:
     interfaces = dev.rpc.get_interface_information()
-    help(interfaces)
-    # print(interfaces)
-    # with open  ("./0-example_output.xml", "w") as f: 
-    #     f.write(etree.tostring(interfaces).decode())
-    # # print(etree.tostring(interfaces))
+    with open  ("./0-example_output.xml", "w") as f: 
+        f.write(etree.tostring(interfaces).decode())
